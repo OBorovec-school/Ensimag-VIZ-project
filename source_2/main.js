b338cd43a66f5ec9e2b338e83055cdd28a56be83
Van Tuan Le <van-tuan.le@grenoble-inp.org>
2018-01-10 17:28:49 +0100

    temporary commit

--- source_2/main.js
+++ source_2/main.js
@@ -1,19 +1,29 @@
-
+var all_candidats = [];
+var data_legend = [{label:"Approved", color: "#00cc00"},
+                    {label: "Non-Approved", color: "#ff6600"},
+                    {label: "Non-Consistency", color: "#002699"}];    
+var height = 200, width = 400;
+d3.selectAll("svg").attr("width", width).attr("height", height).each(function(d,index){
+    //this.style("width", W).style("height", H);
+    var name = this.id;
+    all_candidats[index] = this.id;
+});
+sampleNumerical = [1,2.5,5,10,20];
+  sampleThreshold=d3.scaleThreshold().domain(sampleNumerical).range(colorbrewer.Reds[5]);
+    horizontalLegend = d3.svg.legend().units("Miles").cellWidth(80).cellHeight(25).inputScale(sampleThreshold).cellStepping(100);
+      d3.select("svg").append("g").attr("transform", "translate(50,70)").attr("class", "legend").call(horizontalLegend);
+        sampleCategoricalData = ["Something","Something Else", "Another", "This", "That", "Etc"]
+          sampleOrdinal = d3.scale.category20().domain(sampleCategoricalData);
+            verticalLegend = d3.svg.legend().labelFormat("none").cellPadding(5).orientation("vertical").units("Things in a List").cellWidth(25).cellHeight(18).inputScale(sampleOrdinal).cellStepping(10);
+              d3.select("svg").append("g").attr("transform", "translate(50,140)").attr("class", "legend").call(verticalLegend);
 var svg = d3.selectAll("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    radius = Math.min(width, height) / 2,
-    g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    //width = +svg.attr("width"),
+    //height = +svg.attr("height"),
+    radius = Math.min(width, height) / 2;
 
 var pie = d3.pie()
     .sort(null)
     .value(function(d) { return d.percentage; });
-
-var pie_AV = d3.pie()
-    .sort(null)
-    .value(function(d) { return d; });
 
 var path = d3.arc()
     .outerRadius(radius - 10)
@@ -23,16 +33,9 @@ var label = d3.arc()
     .outerRadius(radius - 40)
     .innerRadius(radius - 40);
 
-
-
-var all_candidats = [], data_AVs = [], path_EVs = {}, gs={};
-d3.selectAll("svg").each(function(d,index){
-    var name = this.id;
-    all_candidats[index] = this.id;
-});
-
 var psv = d3.dsvFormat(";");
 
+var color = {non_approved: "#00cc00" , approved: "#ff6600"};
 
 d3.request("../data/VT1.csv").mimeType("text/plain")
     .response(function(xhr){ return psv.parse(xhr.responseText)})
@@ -55,11 +58,45 @@ d3.request("../data/VT3.csv").mimeType("text/plain")
 
     all_candidats.forEach(function(name){
         d3.select("#"+name).html(null);
+        // For AV chart
+        var data_AV = [{},{}], yes = 0, no=0;
+        data_processed.forEach(function(d){
+            if(d[" AV_"+name]==1) yes++;
+            else no++;
+        });
+        data_AV[0]["percentage"] = no; 
+        data_AV[0]["approved"] = false; 
+        data_AV[1]["percentage"] = yes;
+        data_AV[1]["approved"] = true; 
+        g = d3.select("#"+name).append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        // Approval pie chart
+        var arc = g.selectAll(".arc_AV"+name)
+            .data(pie(data_AV))
+            .enter().append("g")
+            .attr("class", "arc");
+
+        arc.append("path")
+            .attr("d", path)
+            .attr("fill", function(d) { 
+                if(d.data["approved"])
+                    //return "orange"; 
+                    return color.approved; 
+                else
+                    //return "blue";
+                    return color.non_approved;
+            })
+            .style("fill-opacity", 0.2)
+            //.style('stroke', 'black')
+            //.style('stroke-width', '1');
+
+        // for EV 
         var path_EV = d3.arc()
             .outerRadius(function(d1){return d1.data[" EV_"+name]*(radius-10)})
             .innerRadius(0);
-
+    
         var pie_EV = d3.pie()
+            .padAngle(0)
             .sort(function(a,b){
                 if(a[" AV_"+name] > b[" AV_"+name]) return 1;
                 if(a[" AV_"+name] < b[" AV_"+name]) return -1;
@@ -71,7 +108,7 @@ d3.request("../data/VT3.csv").mimeType("text/plain")
 
         g = d3.select("#"+name).append("g")
             .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-        var arc = g.selectAll(".arc_EV"+name)
+        arc = g.selectAll(".arc_EV"+name)
             .data(pie_EV(data_processed))
             .enter().append("g")
             .attr("class", "arc");
@@ -80,34 +117,36 @@ d3.request("../data/VT3.csv").mimeType("text/plain")
             .attr("d", path_EV)
             .attr("fill", function(d) { 
                 if(d.data[" AV_"+name]==1)
-                    return "red"; 
+                    //return "orange"; 
+                    return color.approved; 
                 else
-                    return "blue";
+                    //return "blue";
+                    return color.non_approved;
             });
-        var data_AV = [{},{}], yes = 0, no=0;
-        data_processed.forEach(function(d){
-            if(d[" AV_"+name]==1) yes++;
-            else no++;
-        });
-        data_AV[0]["percentage"] = no; 
-        data_AV[0]["approved"] = false; 
-        data_AV[1]["percentage"] = yes;
-        data_AV[1]["approved"] = true; 
-        // Approval pie chart
-        arc = g.selectAll(".arc_AV"+name)
-            .data(pie(data_AV))
+
+        // For circle of radius 0,5
+        var path_CR = d3.arc()
+            .outerRadius(function(d){
+                if( (d.data[" AV_"+name]==1&&d.data[" EV_"+name]<=0.5) ||
+                    (d.data[" AV_"+name]==0&&d.data[" EV_"+name]>=0.5) ) 
+                    return 0.5*(radius-10);
+                else return 0;
+            })
+            .innerRadius(0);
+
+        g = d3.select("#"+name).append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        arc = g.selectAll(".arc_CR"+name)
+            .data(pie_EV(data_processed))
             .enter().append("g")
             .attr("class", "arc");
 
         arc.append("path")
-            .attr("d", path)
+            .attr("d", path_CR)
             .attr("fill", function(d) { 
-                if(d.data["approved"])
-                    return "red"; 
-                else
-                    return "blue";
+                return "#002699"; 
             })
-            .style("fill-opacity", 0.2);
+            //.style("fill-opacity", );
     });
 });});});
 
